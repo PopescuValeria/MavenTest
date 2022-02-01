d1f77fd835f68ac4bc1e74b211a4b9b63ef43a67
user <popescu.valeria99@gmail.com>
2022-02-01 20:04:15 +0200

    Commit at 01.02.2022 - added a new class NewBranchTest with Scanner class

--- src/main/java/com/testing/CSV/NewBranchTest.java
+++ src/main/java/com/testing/CSV/NewBranchTest.java
@@ -1,4 +1,14 @@
 package com.testing.CSV;
 
+import java.util.Scanner;
+
 public class NewBranchTest {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter name --> ");
+        String  name = scanner.nextLine();
+
+        System.out.println("Hello "+name);
+    }
 }
